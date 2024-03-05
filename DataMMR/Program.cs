a6b53b70e48e718e0cc9b3cc9561ea6721f9c46e
scixing <whs11998@163.com>
2024-03-05 13:18:58 +0800

    添加项目文件。

---
+++ DataMMR/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+int[][] dirs = new int[][]
+{
+    new int[] { 0, 1 },
+    new int[] { 0, -1 },
+    new int[] { 1, 0 },
+    new int[] { -1, 0 },
+    new int[] { 1, -1 },
+    new int[] { -1, 1 },
+};
+
+var data = JsonSerializer.Deserialize<List<List<JsonElement>>>(File.ReadAllText(@"G:\CSharpDev\Learn\MMRProblem\DataMMR\AbilityPanelTest.json"));
+Dictionary<Pos, (int, int)> map = new()
+{
+    { new Pos(0, 0), (0, 0) }
+};
+
+List<int> W = [0];
+int idx = 1;
+foreach (var item in data)
+{
+    map.Add(new Pos(item[7].GetInt32(), item[8].GetInt32()), (idx, item[3].GetInt32()));
+    W.Add(item[3].GetInt32());
+    idx++;
+}
+
+var pointsCnt = data.Count + 1;
+var points = map.Keys.ToArray();
+List<Edge> edges = new();
+
+for (int i = 0; i < pointsCnt; i++)
+{
+    var neighbours = GetNeighbours(points[i]);
+    foreach (var neighbour in neighbours)
+    {
+        var (idx1, w1) = map[points[i]];
+        var (idx2, w2) = map[neighbour];
+        edges.Add(new Edge(idx1, idx2, Math.Abs(w2)));
+    }
+}
+
+Console.WriteLine(pointsCnt);
+foreach (var w in W)
+{
+    Console.Write($"{w} ");
+}
+Console.WriteLine();
+Console.WriteLine(edges.Count);
+foreach (var edge in edges)
+{
+    Console.WriteLine($"{edge.S} {edge.T}");
+}
+
+// // 输出一下边
+// foreach (var edge in edges)
+// {
+//     Console.WriteLine($"{edge.S} -> {edge.T} : {edge.W}");
+// }
+
+
+
+
+
+Pos[] GetNeighbours(Pos pos)
+{
+    List<Pos> res = new();
+    foreach (var dir in dirs)
+    {
+        var newPos = new Pos(pos.X + dir[0], pos.Y + dir[1]);
+        if (map.ContainsKey(newPos))
+        {
+            res.Add(newPos);
+        }
+    }
+    return res.ToArray();
+}
+
+record Pos(int X, int Y);
+/// <summary>
+/// 边
+/// </summary>
+/// <param name="S"></param>
+/// <param name="T"></param>
+/// <param name="W"></param>
+record Edge(int S, int T, int W);
+
---
+++ M1/Program.cs
@@ -0,0 +1,146 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int N = int.Parse(Console.ReadLine());
+
+// 0 ~ N-1节点的权重, 表示到达该点所需要耗费的资源
+int[] W = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+// 边数量
+int M = int.Parse(Console.ReadLine());
+
+List<List<int>> neighbours = new List<List<int>>();
+for (int i = 0; i < N; i++)
+{
+    neighbours.Add(new List<int>());
+}
+
+// 读入边 一个长度为2的数组 代表起点和终点
+List<int[]> edges = new List<int[]>();
+for (int i = 0; i < M; i++)
+{
+    edges.Add(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+    neighbours[edges[i][0]].Add(edges[i][1]);
+}
+
+
+// 利用floyd算法求最短路径
+
+int[,] dis = new int[N, N];
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        dis[i, j] = int.MaxValue;
+    }
+}
+
+for (int i = 0; i < N; i++)
+{
+    dis[i, i] = 0;
+}
+
+foreach (var edge in edges)
+{
+    dis[edge[0], edge[1]] = W[edge[1]];
+    dis[edge[1], edge[0]] = W[edge[0]];
+}
+
+for (int k = 0; k < N; k++)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (dis[i, k] != int.MaxValue && dis[k, j] != int.MaxValue && dis[i, k] + dis[k, j] < dis[i, j])
+            {
+                dis[i, j] = dis[i, k] + dis[k, j];
+            }
+        }
+    }
+}
+
+// 读入起始点和n个终点
+int start = 0;
+int[] ends = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+// 从起始点开始，每次找到一个可以到达的点，这个点可以使所有终点距离当前已到达的点的距离总和最小
+
+HashSet<int> canReach = new HashSet<int>();
+HashSet<int> visited = new HashSet<int>();
+canReach.Add(start);
+
+// 记录每个终点到visited中的任意一点的最短距离
+int[] endDistance = new int[ends.Length];
+for (int i = 0; i < ends.Length; i++)
+{
+    endDistance[i] = int.MaxValue / 1000;
+}
+
+// 输出一下endDistance
+foreach (var d in endDistance)
+{
+    Console.Write($"{d} ");
+}
+Console.WriteLine();
+
+while (endDistance.Any(s => s > 0))
+{
+    int maxReduceDis = 0;
+    int nextNode = -1;
+    int minCost = int.MaxValue;
+    // 从canReach中找到一个点 使所有ends中的点到visited中的任意一点的距离和最小
+    foreach (var node in canReach) // 撕烤维护（？
+    {
+        int reduceDis = 0;
+        for (int i = 0; i < ends.Length; i++)
+        {
+            if (dis[node, ends[i]] < endDistance[i])
+            {
+                reduceDis += endDistance[i] - dis[node, ends[i]];
+                endDistance[i] = dis[node, ends[i]];
+            }
+        }
+        if (reduceDis > maxReduceDis)
+        {
+            maxReduceDis = reduceDis;
+            nextNode = node;
+            minCost = W[node]; // 这个到底值不值还得撕烤
+        }
+        else if (reduceDis ==  maxReduceDis)
+        {
+            if (W[node]< minCost)
+            {
+                nextNode = node;
+                minCost = W[node]; // 这个到底值不值还得撕烤
+            }
+        }
+    }
+
+    // 将这个点加入visited
+    visited.Add(nextNode);
+    canReach.Remove(nextNode);
+
+    System.Console.WriteLine($"nextNode: {nextNode} maxReduceDis: {maxReduceDis}");
+    // 输出一下endDistance
+    foreach (var d in endDistance)
+    {
+        Console.Write($"{d} ");
+    }
+    Console.WriteLine();
+
+    // canReach中加入这个点的邻居
+    foreach (var neighbour in neighbours[nextNode])
+    {
+        if (!visited.Contains(neighbour))
+        {
+            canReach.Add(neighbour);
+        }
+    }
+}
+
+Console.WriteLine(visited.Count);
+foreach (var node in visited)
+{
+    Console.Write($"{node} ");
+}
+
